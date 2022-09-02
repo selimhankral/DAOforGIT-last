cebfe09a0618d1bec66b9976a9f88070083ad628
selimhankral <82326084+selimhankral@users.noreply.github.com>
2022-09-02 21:44:56 +0300

    DAOforreal
    .

---
+++ DAO-Tutorial/hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xcf0662f2458739960bED97444B0BF930B154f888";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
